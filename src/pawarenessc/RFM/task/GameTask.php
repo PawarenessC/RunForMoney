dc7a2ef2987a51d1afe9a66523d86a8229b3ab22
PawarenessC <35727789+Xxawareness@users.noreply.github.com>
2019-06-02 10:40:51 +0900

    Update GameTask.php

--- src/pawarenessc/RFM/task/GameTask.php
+++ src/pawarenessc/RFM/task/GameTask.php
@@ -75,20 +75,39 @@ class GameTask extends Task
 			
 			$owner->Popup("§f残り時間:§l§f{$minutes}§r§b:§r§f§l{$seconds}§r§e \n§r賞金  §d".$win."§b円§r\n     §l§a逃走者 ".$t." §cvs §bハンター ".$h."\n\n\n\n");
 			
-			if($t == 0 or $t < 0)
-			{
-				$owner->msg("§l§bINFO>>§r §a逃走者が全滅しました！");
-				$owner->msg("§l§bINFO>>§r §bハンターの勝利です！");
-				$owner->msg("§l§bINFO>>§r §cハンターは§6{$win}§b円の賞金を手にいれた！");
-  				$owner->endGame();
+				if($t == 0 or $t < 0)
+				{
+					foreach ($players as $player)
+  					{
+						$name = $player->getName();
+						$owner->msg("§l§bINFO>>§r §a逃走者が全滅しました！");
+						$owner->msg("§l§bINFO>>§r §bハンターの勝利です！");
+						$owner->msg("§l§bINFO>>§r §cハンターは§6{$win}§b円の賞金を手にいれた！");
+  						$owner->endGame();
+				
+					if($owner->type[$name] == 2)
+					{
+  						$owner->addMoney($win, $name);
+  					}
+				}
 			}
 			
-			if($h == 0 or $h < 0)
-			{
-				$owner->msg("§l§bINFO>>§r §cハンターが全滅しました！");
-				$owner->msg("§l§bINFO>>§r §b逃走者の勝利です！");
-				$owner->msg("§l§bINFO>>§r §b逃走者が§6{$win}§b円の賞金を手にいれた！");
-  				$owner->endGame();
+				if($h == 0 or $h < 0)
+				{
+					foreach ($players as $player)
+  					{
+						$name = $player->getName();
+						$owner->msg("§l§bINFO>>§r §cハンターが全滅しました！");
+						$owner->msg("§l§bINFO>>§r §b逃走者の勝利です！");
+						$owner->msg("§l§bINFO>>§r §b逃走者が§6{$win}§b円の賞金を手にいれた！");
+  						$owner->endGame();
+					
+					if($owner->type[$name] == 1)
+					{
+  						$owner->addMoney($win, $name);
+  					}	
+					
+				}
 			}
 			
 			switch($owner->gametime)
